ba0245e86c680e44c8338d6eb92db40f7d269abf
Pravanjan Sahu (Digital) <PR20076059@wipro.com>
2019-04-29 19:07:31 +0530

    serviceDiscov

--- integration-test/src/test/java/test/pivotal/pal/tracker/FlowTest.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/FlowTest.java
@@ -1,6 +1,5 @@
 package test.pivotal.pal.tracker;
 
-
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.PathNotFoundException;
 import io.pivotal.pal.tracker.testsupport.TestScenarioSupport;
